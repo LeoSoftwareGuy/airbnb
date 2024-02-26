fafa67c2996e1d91312b5883e2d27db62f12bb0a
Leonid Lobanov <leonid.lobanov@pixster.eu>
2024-02-26 16:53:46 +0200

    properties page

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,51 @@
-import getListings from "./actions/getListings";
-import Container from "./components/Container";
-import EmptyState from "./components/EmptyState";
-import ListingCard from "./components/listings/ListingCards";
-import getCurrentUser from "./actions/getCurrentUser";
+import Container from "@/app/components/Container";
+import ListingCards from "@/app/components/listings/ListingCards";
+import EmptyState from "@/app/components/EmptyState";
 
-export default async function Home() {
-  const listings = await getListings();
+import getListings, { 
+  IListingsParams
+} from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+interface HomeProps {
+  searchParams: IListingsParams
+};
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
-    return <EmptyState showReset />;
+    return (
+        <EmptyState showReset />
+    );
   }
+
   return (
-    <Container>
-      <div
-        className="
-      pt-24
-      grid
-      grid-cols-1
-      sm:grid-cols-2
-      md:grid-cols-3
-      lg:grid-cols-4
-      xl:grid-cols-5
-      2xl:grid-cols-6
-      gap-8"
-      >
-        {listings.map((listing) => (
-          <ListingCard
-            key={listing.id}
-            data={listing}
-            currentUser={currentUser}
-          />
-        ))}
-      </div>
-    </Container>
-  );
+      <Container>
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCards
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+  )
 }
+
+export default Home;
