08f7543db31212168a1795f05e0b76893cc3509a
Leonid Lobanov <leonid.lobanov@pixster.eu>
2024-02-26 22:26:40 +0200

    SearchModal is done for filtering

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,11 @@ import getCurrentUser from "./actions/getCurrentUser";
 import LoginModal from "./components/modal/LoginModel";
 import RegisterModal from "./components/modal/RegisterModal";
 import RentModal from "./components/modal/RentModal";
+import SearchModal from "./components/modal/SearchModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import ToasterProvider from "./providers/ToasterProvider";
+
 
 export const metadata = {
   title: "Airbnb",
@@ -27,6 +29,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <>
           <ToasterProvider />
+          <SearchModal/>
           <RentModal />
           <LoginModal />
           <RegisterModal />
